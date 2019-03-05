8ce0f398f14e0d8e39e515eb404410804bcaf47a
mribera-erre2web <ma58n21lo>
2019-03-05 17:15:00 +0100

    Some fix

--- src/Client/Dispatch.php
+++ src/Client/Dispatch.php
@@ -5,5 +5,87 @@ namespace NexmoPhp\Client;
 
 class Dispatch
 {
+    public $from;
+    public $to;
 
+    public $message = [
+        "content"=>[
+            "type" => "",
+            "text" => ""
+        ]
+    ];
+
+    public function whatsapp(){
+        $this->setStandard();
+        $this->from["type"] = "whatsapp";
+        $this->to["type"] = "whatsapp";
+    }
+
+    public function sms(){
+        $this->setStandard();
+        $this->from["type"] = "sms";
+        $this->to["type"] = "sms";
+    }
+
+    public function viber(){
+        $this->setStandard();
+        $this->from["type"] = "viber";
+        $this->to["type"] = "viber";
+    }
+
+    public function messenger(){
+        $this->setFb();
+        $this->from["type"] = "messenger";
+        $this->to["type"] = "messenger";
+    }
+
+    public function setStandard(){
+        $this->from = [
+            "type"=>"",
+            "number"=>""
+        ];
+        $this->to = [
+            "type"=>"",
+            "number"=>""
+        ];
+    }
+
+    public function setFb(){
+        $this->from = [
+            "type"=>"",
+            "id"=>""
+        ];
+        $this->to = [
+            "type"=>"",
+            "id"=>""
+        ];
+    }
+
+    public function setFrom($from){
+        if($this->from["type"]!="messenger"){
+            $this->from["number"] = $from;
+        }else{
+            $this->from["id"] = $from;
+        }
+    }
+
+    public function setTo($to){
+        if($this->to["type"]!="messenger"){
+            $this->to["number"] = $to;
+        }else{
+            $this->to["id"] = $to;
+        }
+    }
+
+    public function message($type, $text){
+        if($type=="text"){
+            $this->message["content"]["type"] = $type;
+            $this->message["content"]["text"] = $text;
+        }
+    }
+
+    public function __invoke()
+    {
+
+    }
 }
--- test/sendSms.php
+++ test/sendSms.php
@@ -1,16 +1,24 @@
 <?php
-namespace NexmoPhp\Client;
+namespace NexmoPhp\Client\Tests;
 
 $credentials = new Credentials("APIKey", "APISecret");
 
 $client = new Client($credentials);
 
-$message = new Messages();
+$message = new Messages(); // Create single message
 
-$message->sms(); //set message type sms/whatsapp/viber/messenger
+/*CHANNEL OF DESTINATION*/
+$message->sms(); //set message type sms
+//$message->whatsapp(); //set message type whatsapp
+//$message->viber(); //set message type viber
+//$message->messenger(); //set message type messenger
+
+/*BODY OF THE MESSAGE*/
 $message->message("text", "heye"); //set message
+
+/*FROM AND DESTINATION (ID FOR MESSENGER)*/
 $message->setFrom("NUMBER"); //set from number
 $message->setTo("NUMBER"); //set to number
 
-$client->messages(); //set endpoint messages and send
-//$client->dispatch(); //set endpoin dispatch and send
+/*DELIVER*/
+$client->messages($message); //set endpoint messages and send
